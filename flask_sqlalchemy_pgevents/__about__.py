0227ae79a06b43998b97b9fb1e34869463e24c44
Shawn Wallis <shawn.p.wallis@gmail.com>
2018-09-01 13:58:47 -0400

    - adds about module - adds Flask and Flask-SQLAlchemy as requirements

---
+++ flask_sqlalchemy_pgevents/__about__.py
@@ -0,0 +1,12 @@
+"""This module contains information about this package."""
+__title__ = 'Flask-SQLAlchemy-pgevents'
+__summary__ = 'Flask extension for psycopg2-pgevents, via SQLAlchemy.'
+__uri__ = 'https://example.com'
+
+__version__ = '0.1.0'
+
+__author__ = 'Shawn Wallis'
+__email__ = 'shawn.p.wallis@gmail.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 {author}'.format(author=__author__)
