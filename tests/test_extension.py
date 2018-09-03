105980e85bd0b8ab60f8d1c804d9904f402bc164
Shawn Wallis <shawn.p.wallis@gmail.com>
2018-09-02 22:23:12 -0400

    changes test model names

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -63,49 +63,49 @@ class TestExtension:
             assert (pg._connection is None)
 
     def test_get_full_table_name_default_schema(self, app, db):
-        class Animal(db.Model):
-            __tablename__ = 'animal'
+        class Widget(db.Model):
+            __tablename__ = 'widget'
 
             id = db.Column(db.Integer, primary_key=True)
 
         with create_pgevents(app) as pg:
-            expected = 'public.animal'
-            actual = pg._get_full_table_name(Animal)
+            expected = 'public.widget'
+            actual = pg._get_full_table_name(Widget)
 
             assert (expected == actual)
 
     def test_get_full_table_name_custom_schema(self, app, db):
-        class Animal(db.Model):
-            __tablename__ = 'animal'
+        class Gadget(db.Model):
+            __tablename__ = 'gadget'
             __table_args__ = {'schema': 'private'}
             id = db.Column(db.Integer, primary_key=True)
 
         with create_pgevents(app) as pg:
-            expected = 'private.animal'
-            actual = pg._get_full_table_name(Animal)
+            expected = 'private.gadget'
+            actual = pg._get_full_table_name(Gadget)
 
             assert (expected == actual)
 
     def test_install_trigger_for_model(self, app, db):
-        class Animal(db.Model):
-            __tablename__ = 'animal'
+        class Widget(db.Model):
+            __tablename__ = 'widget'
             id = db.Column(db.Integer, primary_key=True)
 
         create_all(db)
 
         with create_pgevents(app) as pg:
             with create_connection(db, raw=True) as conn:
-                trigger_installed_ = trigger_installed(conn, 'animal')
+                trigger_installed_ = trigger_installed(conn, 'widget')
                 assert (trigger_installed_ == False)
 
-                pg._install_trigger_for_model(Animal)
+                pg._install_trigger_for_model(Widget)
 
-                trigger_installed_ = trigger_installed(conn, 'animal')
+                trigger_installed_ = trigger_installed(conn, 'widget')
                 assert (trigger_installed_ == True)
 
     def test_listen_no_identifier(self, app, db):
-        class Animal(db.Model):
-            __tablename__ = 'animal'
+        class Widget(db.Model):
+            __tablename__ = 'widget'
             id = db.Column(db.Integer, primary_key=True)
 
         create_all(db)
@@ -114,13 +114,13 @@ class TestExtension:
             pass
         with create_pgevents(app) as pg:
             with raises(ValueError):
-                pg.listen(Animal, [], event_handler)
+                pg.listen(Widget, [], event_handler)
 
-            assert ('public.animal' not in pg._triggers)
+            assert ('public.widget' not in pg._triggers)
 
     def test_listen_invalid_identifier(self, app, db):
-        class Animal(db.Model):
-            __tablename__ = 'animal'
+        class Widget(db.Model):
+            __tablename__ = 'widget'
             id = db.Column(db.Integer, primary_key=True)
 
         create_all(db)
@@ -129,13 +129,13 @@ class TestExtension:
             pass
         with create_pgevents(app) as pg:
             with raises(ValueError):
-                pg.listen(Animal, ['insert', 'upsert'], event_handler)
+                pg.listen(Widget, ['insert', 'upsert'], event_handler)
 
-            assert ('public.animal' not in pg._triggers)
+            assert ('public.widget' not in pg._triggers)
 
     def test_not_initialized_listen(self, app, db):
-        class Animal(db.Model):
-            __tablename__ = 'animal'
+        class Widget(db.Model):
+            __tablename__ = 'widget'
             id = db.Column(db.Integer, primary_key=True)
 
         create_all(db)
@@ -144,19 +144,19 @@ class TestExtension:
             pass
 
         with create_pgevents() as pg:
-            pg.listen(Animal, ['insert'], event_handler)
+            pg.listen(Widget, ['insert'], event_handler)
 
-            assert ('public.animal' in pg._triggers)
-            trigger = pg._triggers['public.animal'][0]
+            assert ('public.widget' in pg._triggers)
+            trigger = pg._triggers['public.widget'][0]
             assert (trigger.installed == False)
 
             with create_connection(db, raw=True) as conn:
-                trigger_installed_ = trigger_installed(conn, 'animal')
+                trigger_installed_ = trigger_installed(conn, 'widget')
                 assert (trigger_installed_ == False)
 
     def test_listen(self, app, db):
-        class Animal(db.Model):
-            __tablename__ = 'animal'
+        class Widget(db.Model):
+            __tablename__ = 'widget'
             id = db.Column(db.Integer, primary_key=True)
 
         create_all(db)
@@ -165,23 +165,23 @@ class TestExtension:
             pass
 
         with create_pgevents(app) as pg:
-            pg.listen(Animal, ['insert'], event_handler)
+            pg.listen(Widget, ['insert'], event_handler)
 
-            assert ('public.animal' in pg._triggers)
-            assert (len(pg._triggers['public.animal']) == 1)
-            trigger = pg._triggers['public.animal'][0]
+            assert ('public.widget' in pg._triggers)
+            assert (len(pg._triggers['public.widget']) == 1)
+            trigger = pg._triggers['public.widget'][0]
             assert (trigger.installed == True)
-            assert (trigger.target == Animal)
+            assert (trigger.target == Widget)
             assert (trigger.events == {'insert'})
             assert (trigger.fn == event_handler)
 
             with create_connection(db, raw=True) as conn:
-                trigger_installed_ = trigger_installed(conn, 'animal')
+                trigger_installed_ = trigger_installed(conn, 'widget')
                 assert (trigger_installed_ == True)
 
     def test_listen_one_model_multiple_triggers(self, app, db):
-        class Animal(db.Model):
-            __tablename__ = 'animal'
+        class Widget(db.Model):
+            __tablename__ = 'widget'
             id = db.Column(db.Integer, primary_key=True)
 
         create_all(db)
@@ -193,24 +193,24 @@ class TestExtension:
             pass
 
         with create_pgevents(app) as pg:
-            pg.listen(Animal, ['insert', 'update'], event_handler1)
-            pg.listen(Animal, ['delete'], event_handler2)
+            pg.listen(Widget, ['insert', 'update'], event_handler1)
+            pg.listen(Widget, ['delete'], event_handler2)
 
-            assert ('public.animal' in pg._triggers)
-            assert (len(pg._triggers['public.animal']) == 2)
+            assert ('public.widget' in pg._triggers)
+            assert (len(pg._triggers['public.widget']) == 2)
 
-            trigger = pg._triggers['public.animal'][0]
+            trigger = pg._triggers['public.widget'][0]
             assert (trigger.installed == True)
-            assert (trigger.target == Animal)
+            assert (trigger.target == Widget)
             assert (trigger.events == {'insert', 'update'})
             assert (trigger.fn == event_handler1)
 
-            trigger = pg._triggers['public.animal'][1]
+            trigger = pg._triggers['public.widget'][1]
             assert (trigger.installed == True)
-            assert (trigger.target == Animal)
+            assert (trigger.target == Widget)
             assert (trigger.events == {'delete'})
             assert (trigger.fn == event_handler2)
 
             with create_connection(db, raw=True) as conn:
-                trigger_installed_ = trigger_installed(conn, 'animal')
+                trigger_installed_ = trigger_installed(conn, 'widget')
                 assert (trigger_installed_ == True)
