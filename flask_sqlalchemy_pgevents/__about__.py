55eb70cfb306b5aafec64c3d03f6aac240fbc448
Shawn Wallis <shawn.p.wallis@gmail.com>
2018-09-16 15:37:30 -0400

    changes version to 0.1.1

--- flask_sqlalchemy_pgevents/__about__.py
+++ flask_sqlalchemy_pgevents/__about__.py
@@ -3,7 +3,7 @@ __title__ = 'Flask-SQLAlchemy-PGEvents'
 __summary__ = 'Flask extension for psycopg2-pgevents, using SQLAlchemy.'
 __uri__ = 'https://github.com/shawalli/flask-sqlalchemy-pgevents'
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __author__ = 'Shawn Wallis'
 __email__ = 'shawn.p.wallis@gmail.com'
